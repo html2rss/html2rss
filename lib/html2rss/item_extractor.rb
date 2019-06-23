22515a5e8ae222220ff854467eb91c1e752be9a3
Gil Desmarais <git@desmarais.de>
2019-06-23 14:41:11 +0200

    feat: support attributes without selector, fallback to root element then

--- lib/html2rss/item_extractor.rb
+++ lib/html2rss/item_extractor.rb
@@ -1,10 +1,15 @@
 module Html2rss
   module ItemExtractor
-    TEXT = proc { |xml, options| xml.css(options['selector'])&.text&.strip }
-    ATTRIBUTE = proc { |xml, options| xml.css(options['selector']).attr(options['attribute']).to_s }
+    TEXT = proc { |xml, options|
+      element(xml, options)&.text&.strip&.split&.join(' ')
+    }
+
+    ATTRIBUTE = proc { |xml, options|
+      element(xml, options).attr(options['attribute']).to_s
+    }
 
     HREF = proc { |xml, options|
-      href = xml.css(options['selector']).attr('href').to_s
+      href = element(xml, options).attr('href').to_s
       path, query = href.split('?')
 
       if href.start_with?('http')
@@ -18,8 +23,15 @@ module Html2rss
       uri
     }
 
-    HTML = proc { |xml, options| xml.css(options['selector']).to_s }
+    HTML = proc { |xml, options|
+      element(xml, options).to_s
+    }
+
     STATIC = proc { |_xml, options| options['static'] }
     CURRENT_TIME = proc { |_xml, _options| Time.new }
+
+    def self.element(xml, options)
+      options['selector'] ? xml.css(options['selector']) : xml
+    end
   end
 end
