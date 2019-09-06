cecbe5eb7b8586f036169480cd009c8be69b4f22
Gil Desmarais <gildesmarais@users.noreply.github.com>
2019-09-06 21:39:25 +0200

    feat(parse_time): support setting of a time_zone (#31)
    * feat(parse_time): support setting of a time_zone
    
    Fixes #19.
    
    * use activesupport <6 to keep support for older rubies
    
    * in the zone

--- spec/html2rss/attribute_post_processors/parse_time_spec.rb
+++ spec/html2rss/attribute_post_processors/parse_time_spec.rb
@@ -1,7 +1,9 @@
 RSpec.describe Html2rss::AttributePostProcessors::ParseTime do
   context 'with String value' do
-    subject { described_class.new('2019-07-01 12:00', {}).get }
+    subject { described_class.new('2019-07-01 12:00', config: config).get }
 
-    it { is_expected.to eq 'Mon, 01 Jul 2019 12:00:00 +0000' }
+    let(:config) { Html2rss::Config.new(channel: { time_zone: 'Europe/London' }) }
+
+    it { is_expected.to eq 'Mon, 01 Jul 2019 12:00:00 +0100' }
   end
 end
