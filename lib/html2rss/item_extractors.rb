1943839b5d4d6d370ebdbedb329214d27bcbb179
Gil Desmarais <gildesmarais@users.noreply.github.com>
2019-11-01 20:14:19 +0100

    refactor: extract helper method to Utils from extractor & post processor (#61)

--- lib/html2rss/item_extractors.rb
+++ lib/html2rss/item_extractors.rb
@@ -2,13 +2,11 @@ module Html2rss
   ##
   # Provides a namespace for item extractors.
   module ItemExtractors
-    DEFAULT = 'Text'.freeze
+    DEFAULT = 'text'.freeze
 
     def self.get_extractor(name)
       @get_extractor ||= Hash.new do |extractors, key|
-        camel_cased_name = (key || DEFAULT).split('_').map(&:capitalize).join
-        class_name = ['Html2rss', 'ItemExtractors', camel_cased_name].join('::')
-        extractors[key] = Object.const_get(class_name)
+        extractors[key] = Utils.get_class_from_name(key || DEFAULT, 'ItemExtractors')
       end
 
       @get_extractor[name]
