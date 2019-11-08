bc8ecbb9623ce08a6cd067da1cb5fd0a996a9d40
Gil Desmarais <gildesmarais@users.noreply.github.com>
2019-11-08 20:59:34 +0100

    feat(config): improve generation of channel.title from channel.url (#68)

---
+++ spec/html2rss/config_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe Html2rss::Config do
+  describe '#title' do
+    subject { described_class.new(feed_config).title }
+
+    context 'with channel.title present' do
+      let(:feed_config) { { channel: { title: 'An example channel' } } }
+
+      it { is_expected.to eq feed_config[:channel][:title] }
+    end
+
+    context 'without channel.url having path' do
+      let(:feed_config) { { channel: { url: 'http://www.example.com' } } }
+
+      it { is_expected.to eq 'www.example.com' }
+    end
+
+    context 'with channel.url having path' do
+      let(:feed_config) { { channel: { url: 'http://www.example.com/news' } } }
+
+      it { is_expected.to eq 'www.example.com: News' }
+    end
+  end
+end
