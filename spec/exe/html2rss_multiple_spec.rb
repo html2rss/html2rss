99cfb380f8e3bc16092421db680cfab3d49c1a2a
Gil Desmarais <git@desmarais.de>
2021-02-18 17:16:33 +0100

    first specs

--- spec/exe/html2rss_multiple_spec.rb
+++
@@ -1,2 +0,0 @@
-# frozen_string_literal: true
-# TODO: `exe/html2rss feed spec/cli.multiple.test.yml nuxt-releases`
--- spec/exe/html2rss_single_spec.rb
+++
@@ -1,2 +0,0 @@
-# frozen_string_literal: true
-# TODO: `exe/html2rss feed spec/cli.single.test.yml`
