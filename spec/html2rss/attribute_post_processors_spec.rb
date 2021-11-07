300f38d0b950be220cf4469bfae462e292d422d2
Gil Desmarais <git@desmarais.de>
2021-11-07 18:06:15 +0100

    fix(post_processors): incorrect name used for html_to_markdown

---
+++ spec/html2rss/attribute_post_processors_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe Html2rss::AttributePostProcessors do
+  describe '::NAME_TO_CLASS' do
+    specify(:aggregate_failures) do
+      expect(described_class::NAME_TO_CLASS).to be_a(Hash)
+      expect(described_class::NAME_TO_CLASS).to include(
+        :gsub, :html_to_markdown, :markdown_to_html, :parse_time, :parse_uri, :sanitize_html, :substring, :template
+      )
+    end
+  end
+
+  describe '.get_processor' do
+    it { expect(described_class.get_processor(:gsub)).to be(Html2rss::AttributePostProcessors::Gsub) }
+
+    it {
+      expect do
+        described_class.get_processor(:html_to_mark)
+      end.to raise_error(Html2rss::AttributePostProcessors::UnknownPostProcessorName)
+    }
+  end
+end
