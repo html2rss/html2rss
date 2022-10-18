ff293aad1f04a4b739b3345755588f17e55399f0
Gil Desmarais <git@desmarais.de>
2022-10-18 19:32:32 +0200

    dev: move specs to a better place

--- spec/html2rss/config/channel_spec.rb
+++ spec/html2rss/config/channel_spec.rb
@@ -10,4 +10,32 @@ RSpec.describe Html2rss::Config::Channel do
       ).to be_a(Set).and include 'section', 'something'
     end
   end
+
+  describe '#url' do
+    subject { described_class.new(hash, params:).url }
+
+    let(:params) { {} }
+
+    context 'with non-ascii url and without dynamic parameters' do
+      let(:hash) do
+        { url: 'https://例子.測試/23/' }
+      end
+
+      it do
+        expect(subject).to eq Addressable::URI.parse('https://例子.測試/23/')
+      end
+    end
+
+    context 'with non-ascii url and with dynamic parameters' do
+      let(:hash) do
+        { url: 'https://例子.測試/%<id>s/' }
+      end
+
+      let(:params) { { id: 42 } }
+
+      it do
+        expect(subject).to eq Addressable::URI.parse('https://例子.測試/42/')
+      end
+    end
+  end
 end
