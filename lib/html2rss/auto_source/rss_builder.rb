731c048e26f02b1ec08afc1c9609cc1c46078a36
Gil Desmarais <git@desmarais.de>
2024-05-05 15:48:55 +0200

    feat(auto_source): handle images in semantic_html extractor

--- lib/html2rss/auto_source/rss_builder.rb
+++ lib/html2rss/auto_source/rss_builder.rb
@@ -31,16 +31,43 @@ module Html2rss
 
       def make_items(articles, maker)
         articles.each do |article|
-          maker.items.new_item do |item|
-            item.id = article[:id]
-            item.title = article[:headline]
-            item.description = article[:description]
-            item.link = Html2rss::Utils.build_absolute_url_from_relative(article[:link], url)
+          maker.items.new_item do |item_maker|
+            add_guid(article, item_maker)
+            add_image(article, item_maker)
+
+            item_maker.title = article[:title]
+            item_maker.description = article[:description]
+            item_maker.pubDate = article[:published_at]
+
+            item_maker.link = clean_url(article[:url])
           end
         end
       end
 
       private
+
+      def clean_url(url)
+        link = Html2rss::Utils.build_absolute_url_from_relative(url, @url)
+        Html2rss::Utils.sanitize_url(link)
+      end
+
+      def add_guid(article, maker)
+        guid = maker.guid
+        guid.content = generate_guid(article)
+        guid.isPermaLink = false
+      end
+
+      def generate_guid(article)
+        Digest::SHA1.hexdigest [url, article[:id] || article[:url].gsub(url, '')].join('|')
+      end
+
+      def add_image(article, maker)
+        return unless article[:image]
+
+        maker.enclosure.url = clean_url(article[:image])
+        maker.enclosure.type = Html2rss::Utils.guess_content_type_from_url(article[:image])
+        maker.enclosure.length = 0
+      end
 
       attr_reader :channel, :articles, :url
     end
--- lib/html2rss/auto_source/semantic_html.rb
+++ lib/html2rss/auto_source/semantic_html.rb
@@ -3,13 +3,16 @@
 module Html2rss
   class AutoSource
     ##
-    # Extracts articles by looking for <script type="application/ld+json"> tag.
+    # Extracts articles by looking for <article> tags containing an <a href> tag.
+    # An article is not considered an article without having an URL.
     #
     # See:
-    # 1. https://schema.org/NewsArticle
-    # 2. https://developers.google.com/search/docs/appearance/structured-data/article#microdata
+    # 1. https://developer.mozilla.org/en-US/docs/Web/HTML/Element/article
     class SemanticHtml
-      TAG_SELECTOR = 'article a[href]'
+      ANCHOR_TAG_SELECTOR = 'article :not(article) a[href]'
+
+      # TODO: also handle <h2><a href>...</a></h2> as article
+      # TODO: also handle <X class="article"><a href>...</a></X> as article
 
       def initialize(parsed_body)
         @parsed_body = parsed_body
@@ -17,12 +20,16 @@ module Html2rss
 
       attr_reader :parsed_body
 
+      def self.articles?(parsed_body)
+        parsed_body.css(ANCHOR_TAG_SELECTOR).any?
+      end
+
       ##
       # @return [Array<Hash>] the extracted articles
       def call
-        anchors = parsed_body.css(TAG_SELECTOR)
+        anchors = parsed_body.css(ANCHOR_TAG_SELECTOR)
 
-        anchors.map do |anchor|
+        anchors.filter_map do |anchor|
           article_tag = anchor.parent
 
           while (name = article_tag.name) != 'article'
@@ -35,64 +42,101 @@ module Html2rss
         end
       end
 
-      def self.articles?(parsed_body)
-        parsed_body.css(TAG_SELECTOR).any?
-      end
+      def extract_article(article)
+        # TODO: extract the article into a separate class, get rid of passing `article` around to methods
 
-      def extract_article(article) # rubocop:disable Metrics/MethodLength
         heading = heading(article)
 
-        title = title(heading)
-        link = link(article, heading)
-        description = description(article, heading)
-
         {
-          id: nil,
-          title:,
-          link:,
-          image: article.css('img')&.first&.[]('src'),
-          description:
+          id: id(article),
+          title: title(article, heading),
+          url: url(article, heading),
+          image: image(article),
+          description: description(article, heading)
         }
       end
 
+      HEADING_TAGS_SELECTOR = 'h1, h2, h3, h4, h5, h6'
+      NOT_HEADLINE_SELECTOR = HEADING_TAGS_SELECTOR.split(',')
+                                                   .map { |selector| ":not(#{selector})" }
+                                                   .join(', ')
+                                                   .freeze
+      ##
+      # Finds the heading tag of an article.
+      #
+      # If multiple headings are found, the smallest heading tag (h1 is smaller than h2) is returned.
+      # If multiple tags of the same size are found, the one with the longest text is returned.
+      # If no heading is found, nil is returned.
+      #
+      # @return [Nokogiri::XML::Element, nil] a heading tag or nil if none found
       def heading(article)
-        heading_tags = article.css('h1, h2, h3, h4, h5, h6').group_by(&:name)
-        lowest_heading_tag_name = heading_tags.keys.min
+        heading_tags = article.css(HEADING_TAGS_SELECTOR).group_by(&:name)
 
-        heading_tags[lowest_heading_tag_name].max_by { |h| h.text.size }
+        return if heading_tags.empty?
+
+        heading_tags[heading_tags.keys.min].max_by { |h| h.text.size }
       end
 
-      def title(heading) = extract_text(heading)
+      # @return [String, nil]
+      def id(article)
+        if (id = article['id'] && !id.empty?)
+          id
+        else
+          article.css('*[id]:not([id=""])')&.map { |tag| tag['id'] }&.first
+        end
+      end
 
+      def title(article, heading)
+        if heading && (title = extract_text(heading))
+          title
+        else
+          article.css(NOT_HEADLINE_SELECTOR)
+                 .filter_map { |tag| extract_text(tag) }
+                 .max_by(&:size)
+        end
+      end
+
+      # @return [String, nil] the text of the tag or nil if empty
       def extract_text(tag, separator: ' ')
         text = case tag.children.size
                when 0
                  tag.text
                when (1..)
-                 tag.children.filter_map { |tag| extract_text(tag) }.join(separator)
-               end
+                 tag.children.filter_map { |tag| extract_text(tag) }.uniq.join(separator)
+               end.dup
 
-        text.gsub(/\s+/, ' ').strip
+        text.gsub!(/\s+/, ' ')
+        text.strip!
+        text.empty? ? nil : text
       end
 
-      # Assumes a link closer to the headline is the correct one.
-      # Falls back to first link in article.
-      def link(article, heading = nil)
-        first_link = article.css('a').first['href']
-
-        if !heading && (down = heading.css('a').first['href'])
+      # Assumes a URL closer to the headline is the correct one.
+      # Falls back to first URL in article.
+      def url(article, heading = nil)
+        if !heading && (down = heading&.css('a')&.first&.[]('href'))
           down
         else
-          first_link
+          article.css('a').first['href']
         end
       end
 
+      def image(article)
+        # TODO: also try <picture><source srcset> (when an <img> is missing)
+        article.css('img[src]')&.first&.[]('src')
+      end
+
+      # @return [String, nil] the description of the article or nil if empty
       def description(article, heading = nil)
-        text = extract_text(article.css('p'))
+        text = extract_text(article.css('p, span'), separator: '<br>')
 
-        return text if (text && text != '') || !heading
+        return text if text || !heading
 
-        extract_text(article).gsub(/\s+/, ' ').gsub(extract_text(heading), '').strip
+        # Get all text of the article, but remove the title
+        description = extract_text(article).gsub(title(article, heading), '')
+
+        description.strip!
+
+        description.empty? ? nil : description
       end
     end
   end
