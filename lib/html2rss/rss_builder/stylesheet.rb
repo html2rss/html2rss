1647c5739689e38e0d229e0d322aa8086276e38e
Gil Desmarais <git@desmarais.de>
2024-07-19 18:08:19 +0200

    refactor(rss_builder): improve code structure
    Reorganized RSS Builder methods into separate functions for better readability and maintainability. Added private method declarations.

--- lib/html2rss/rss_builder/stylesheet.rb
+++ lib/html2rss/rss_builder/stylesheet.rb
@@ -6,20 +6,32 @@ module Html2rss
     # Adds XML stylesheet tags (with the provided maker).
     class Stylesheet
       ##
-      # Adds the stylesheet xml tags to the RSS.
+      # Adds the stylesheet XML tags to the RSS.
       #
-      # @param maker [RSS::Maker::RSS20]
-      # @param stylesheets [Array<Config::Stylesheet>]
-      # @return nil
+      # @param maker [RSS::Maker::RSS20] RSS maker object.
+      # @param stylesheets [Array<Html2rss::Config::Stylesheet>] Array of stylesheet configurations.
+      # @return [nil]
       def self.add(maker, stylesheets)
         stylesheets.each do |stylesheet|
-          maker.xml_stylesheets.new_xml_stylesheet do |xss|
-            xss.href = stylesheet.href
-            xss.type = stylesheet.type
-            xss.media = stylesheet.media
-          end
+          add_stylesheet(maker, stylesheet)
         end
       end
+
+      ##
+      # Adds a single Stylesheet to the RSS.
+      #
+      # @param maker [RSS::Maker::RSS20] RSS maker object.
+      # @param stylesheet [Html2rss::Config::Stylesheet] Stylesheet configuration.
+      # @return [nil]
+      def self.add_stylesheet(maker, stylesheet)
+        maker.xml_stylesheets.new_xml_stylesheet do |xss|
+          xss.href = stylesheet.href
+          xss.type = stylesheet.type
+          xss.media = stylesheet.media
+        end
+      end
+
+      private_class_method :add_stylesheet
     end
   end
 end
