19a43f850167ec581c8b2811f5ab95c3128a2501
Gil Desmarais <git@desmarais.de>
2024-08-10 13:45:26 +0200

    refactor(sanitize_html): inherit from Base and add args validation
    Reorganized method order and added a validation method. Updated test for new inheritance structure.

--- spec/html2rss/attribute_post_processors/sanitize_html_spec.rb
+++ spec/html2rss/attribute_post_processors/sanitize_html_spec.rb
@@ -3,17 +3,20 @@
 RSpec.describe Html2rss::AttributePostProcessors::SanitizeHtml do
   subject { described_class.new(html, config:).get }
 
-  let(:config) do
-    Html2rss::Config.new(
-      channel: { title: 'Example: questions', url: 'https://example.com/questions' },
-      selectors: {
-        items: { selector: '#questions > ul > li' },
-        title: { selector: 'a' },
-        link: { selector: 'a', extractor: 'href' }
-      }
-    )
+  let(:sanitzed_html) do
+    [
+      "Breaking news: I'm a deprecated tag ",
+      '<div> ',
+      '<a href="https://example.com/lol.gif" rel="nofollow noopener noreferrer" target="_blank">',
+      '<img src="https://example.com/lol.gif" alt="An animal looking cute" referrer-policy="no-referrer">',
+      '</a> ',
+      '<a href="http://example.com" title="foo" rel="nofollow noopener noreferrer" target="_blank">',
+      'example.com</a> ',
+      '<a href="https://example.com/article-123" rel="nofollow noopener noreferrer" target="_blank">',
+      'Click here!</a> ',
+      '</div>'
+    ].join
   end
-
   let(:html) do
     <<~HTML
       <html lang="en">
@@ -32,21 +35,18 @@ RSpec.describe Html2rss::AttributePostProcessors::SanitizeHtml do
       </html>
     HTML
   end
-
-  let(:sanitzed_html) do
-    [
-      "Breaking news: I'm a deprecated tag ",
-      '<div> ',
-      '<a href="https://example.com/lol.gif" rel="nofollow noopener noreferrer" target="_blank">',
-      '<img src="https://example.com/lol.gif" alt="An animal looking cute" referrer-policy="no-referrer">',
-      '</a> ',
-      '<a href="http://example.com" title="foo" rel="nofollow noopener noreferrer" target="_blank">',
-      'example.com</a> ',
-      '<a href="https://example.com/article-123" rel="nofollow noopener noreferrer" target="_blank">',
-      'Click here!</a> ',
-      '</div>'
-    ].join
+  let(:config) do
+    Html2rss::Config.new(
+      channel: { title: 'Example: questions', url: 'https://example.com/questions' },
+      selectors: {
+        items: { selector: '#questions > ul > li' },
+        title: { selector: 'a' },
+        link: { selector: 'a', extractor: 'href' }
+      }
+    )
   end
+
+  it { expect(described_class).to be < Html2rss::AttributePostProcessors::Base }
 
   it { is_expected.to eq sanitzed_html }
 end
