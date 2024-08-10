110af420e5477eb1558728ef3bff95e5cb838bc1
Gil Desmarais <git@desmarais.de>
2024-08-10 13:45:45 +0200

    refactor(substring): inherit from Base and add args validation
    Refactored Substring to inherit from Base, added argument validation, and improved range extraction logic. Updated specs accordingly.

--- spec/html2rss/attribute_post_processors/substring_spec.rb
+++ spec/html2rss/attribute_post_processors/substring_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe Html2rss::AttributePostProcessors::Substring do
+  it { expect(described_class).to be < Html2rss::AttributePostProcessors::Base }
+
   context 'with end' do
     subject { described_class.new('Foo bar and baz', options: { start: 4, end: 6 }).get }
 
@@ -11,5 +13,43 @@ RSpec.describe Html2rss::AttributePostProcessors::Substring do
     subject { described_class.new('foobarbaz', options: { start: 3 }).get }
 
     it { is_expected.to eq 'barbaz' }
+  end
+
+  describe '#range' do
+    subject { described_class.new('value', options:) }
+
+    context 'when start and end options are provided' do
+      let(:options) { { start: 2, end: 4 } }
+
+      it 'returns the correct range' do
+        expect(subject.range).to eq(2..4)
+      end
+    end
+
+    context 'when only start option is provided' do
+      let(:options) { { start: 3 } }
+
+      it 'returns the range from start index to the end of the string' do
+        expect(subject.range).to eq(3..)
+      end
+    end
+
+    context 'when start and end options are equal' do
+      let(:options) { { start: 2, end: 2 } }
+
+      it 'raises an ArgumentError' do
+        expect do
+          subject.range
+        end.to raise_error(ArgumentError, 'The `start` value must be unequal to the `end` value.')
+      end
+    end
+
+    context 'when start option is missing' do
+      let(:options) { { end: 4 } }
+
+      it 'raises an error' do
+        expect { subject.range }.to raise_error(Html2rss::AttributePostProcessors::InvalidType, /but is: NilClass/)
+      end
+    end
   end
 end
