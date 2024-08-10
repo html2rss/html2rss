ab3e3bbe0188c4de28ddcbb71dc4e68ddb0a764f
Gil Desmarais <git@desmarais.de>
2024-08-10 13:46:37 +0200

    feat(selectors): add InvalidSelectorName error and validate keys
    Introduce `InvalidSelectorName` error for invalid selector names. Warn about additional keys not in the whitelist. Enhanced specs with new test cases.

--- spec/html2rss/config/selectors_spec.rb
+++ spec/html2rss/config/selectors_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe Html2rss::Config::Selectors do
-  let(:instance) { described_class.new({ items: { selector: '' }, categories: ['name', 'name', nil], name: {} }) }
+  subject(:instance) { described_class.new(config) }
+
+  let(:config) { { items: { selector: '' }, categories: ['name', 'name', nil], name: {} } }
 
   describe '#category_selector_names' do
     subject { instance.category_selector_names }
@@ -20,5 +22,24 @@ RSpec.describe Html2rss::Config::Selectors do
 
     it { is_expected.to be_a(Set) & include(:categories, :name) }
     it { is_expected.not_to include(described_class::ITEMS_SELECTOR_NAME) }
+  end
+
+  describe '#selector' do
+    let(:config) { { items: { selector: '.item' }, categories: ['name'], name: { selector: '.name' } } }
+
+    context 'when a valid selector name is provided' do
+      it 'returns a Selector object with the selector', :aggregate_failures do
+        selector = instance.selector(:name)
+
+        expect(selector).to be_a(Html2rss::Config::Selectors::Selector)
+        expect(selector.selector).to eq('.name')
+      end
+    end
+
+    context 'when an invalid selector name is provided' do
+      it 'raises an InvalidSelectorName error' do
+        expect { instance.selector(:invalid) }.to raise_error(Html2rss::Config::Selectors::InvalidSelectorName)
+      end
+    end
   end
 end
