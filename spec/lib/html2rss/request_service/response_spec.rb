2f1f01df34d8ff24db7945b9ae13b59ec86400a0
Gil Desmarais <git@desmarais.de>
2025-01-17 12:53:21 +0100

    spec(request_service): add tests for #parsed_body
    This update introduces comprehensive tests for the `parsed_body` method, ensuring correct parsing and handling of HTML comments, JSON structures, and error raising for unsupported content types. This enhances the robustness of response content processing in the RequestService module.

--- spec/lib/html2rss/request_service/response_spec.rb
+++ spec/lib/html2rss/request_service/response_spec.rb
@@ -3,16 +3,65 @@
 require 'spec_helper'
 
 RSpec.describe Html2rss::RequestService::Response do
-  describe '#headers' do
-    subject(:returned_headers) { described_class.new(body: '', headers:, url: Addressable::URI.parse('https://example.com')).headers }
+  subject(:instance) { described_class.new(body:, headers:, url: Addressable::URI.parse('https://example.com')) }
 
-    let(:headers) do
-      { key: 42 }
-    end
+  describe '#headers' do
+    subject(:returned_headers) { instance.headers }
+
+    let(:body) { nil }
+    let(:headers) { { key: 42 } }
 
     it 'returns hash w/ string keys', :aggregate_failures do
       expect(returned_headers).to eq('key' => 42)
       expect(returned_headers).not_to be headers
+    end
+  end
+
+  describe '#parsed_body' do
+    subject(:parsed_body) { instance.parsed_body }
+
+    context 'when the response is HTML' do
+      let(:body) do
+        <<-HTML
+      <html>
+        <body>
+          <!-- This is a comment -->
+          <div>Hello World</div>
+        </body>
+      </html>
+        HTML
+      end
+      let(:headers) { { 'content-type' => 'text/html' } }
+
+      it { expect(parsed_body).to be_frozen }
+
+      it 'parses the body and removes comments', :aggregate_failures do
+        expect(parsed_body.at_xpath('//comment()')).to be_nil
+        expect(parsed_body.at_css('div').text).to eq('Hello World')
+      end
+    end
+
+    context 'when the response is JSON' do
+      let(:body) { '{"key": "value"}' }
+      let(:headers) { { 'content-type' => 'application/json' } }
+
+      it { expect(parsed_body).to be_frozen }
+
+      it 'parses the body as JSON' do
+        expect(parsed_body).to eq({ key: 'value' })
+      end
+    end
+
+    context 'when the response content type is not supported' do
+      let(:body) { 'Some unsupported content' }
+      let(:headers) { { 'content-type' => 'text/plain' } }
+
+      it 'raises an UnsupportedResponseContentType error' do
+        expect do
+          parsed_body
+        end.to raise_error(Html2rss::RequestService::UnsupportedResponseContentType,
+                           'Unsupported content type: text/plain')
+      end
     end
   end
 end
