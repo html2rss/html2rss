5d940f55ec5f92faafe092dcae9372bb9b2be419
Gil Desmarais <git@desmarais.de>
2026-08-10 12:01:52 +0200

    refactor(config): unify validation contracts under Config namespace

--- lib/html2rss/auto_source.rb
+++ lib/html2rss/auto_source.rb
@@ -45,41 +45,8 @@ module Html2rss
       cleanup: Cleanup::DEFAULT_CONFIG
     }.freeze
 
-    SCRAPER_CONFIG = proc do
-      optional(:wordpress_api).hash do
-        optional(:enabled).filled(:bool)
-      end
-      optional(:schema).hash do
-        optional(:enabled).filled(:bool)
-      end
-      optional(:microdata).hash do
-        optional(:enabled).filled(:bool)
-      end
-      optional(:json_state).hash do
-        optional(:enabled).filled(:bool)
-      end
-      optional(:semantic_html).hash do
-        optional(:enabled).filled(:bool)
-        optional(:fallback_anchorless).filled(:bool)
-      end
-      optional(:html).hash do
-        optional(:enabled).filled(:bool)
-        optional(:minimum_selector_frequency).filled(:integer, gt?: 0)
-        optional(:use_top_selectors).filled(:integer, gt?: 0)
-        optional(:fallback_anchorless).filled(:bool)
-      end
-    end.freeze
-    private_constant :SCRAPER_CONFIG
-
     # Runtime source of truth for validating auto-source config values.
-    Config = Dry::Schema.Params do
-      optional(:scraper).hash(&SCRAPER_CONFIG)
-
-      optional(:cleanup).hash do
-        optional(:keep_different_domain).filled(:bool)
-        optional(:min_words_title).filled(:integer, gt?: 0)
-      end
-    end
+    Config = Html2rss::Config::AutoSourceContract
 
     ##
     # @param response [Html2rss::RequestService::Response] initial page response
---
+++ lib/html2rss/config/auto_source_contract.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'dry-validation'
+
+module Html2rss
+  class Config
+    # Runtime source of truth for validating auto-source config values.
+    AutoSourceContract = Dry::Schema.Params do
+      optional(:scraper).hash do
+        optional(:wordpress_api).hash do
+          optional(:enabled).filled(:bool)
+        end
+        optional(:schema).hash do
+          optional(:enabled).filled(:bool)
+        end
+        optional(:microdata).hash do
+          optional(:enabled).filled(:bool)
+        end
+        optional(:json_state).hash do
+          optional(:enabled).filled(:bool)
+        end
+        optional(:semantic_html).hash do
+          optional(:enabled).filled(:bool)
+          optional(:fallback_anchorless).filled(:bool)
+        end
+        optional(:html).hash do
+          optional(:enabled).filled(:bool)
+          optional(:minimum_selector_frequency).filled(:integer, gt?: 0)
+          optional(:use_top_selectors).filled(:integer, gt?: 0)
+          optional(:fallback_anchorless).filled(:bool)
+        end
+      end
+
+      optional(:cleanup).hash do
+        optional(:keep_different_domain).filled(:bool)
+        optional(:min_words_title).filled(:integer, gt?: 0)
+      end
+    end
+  end
+end
